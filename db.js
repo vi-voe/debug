6512cd652501f03da2f6f856f9e4cd04aa6b0c64
ext_vvoevodkina <vivoe@yandex.ru>
2021-05-19 19:21:49 +0300

    init: setup project

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const Sequelize = require('sequelize');
-                                //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
+require('dotenv').config();
+
+const sequelize = new Sequelize(process.env.DB, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 })
 
